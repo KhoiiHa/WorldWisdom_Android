bec65d4d825c09c63ab3e1202e4c2664f39e5886
michaelSyntax <michael.karbe@syntax-institut.de>
2024-10-01 15:33:34 +0200

    Some minor refactoring in gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,53 +54,53 @@ android {
 
 dependencies {
 
-    implementation("com.google.android.material:material:1.9.0")
+    implementation(libs.material.v190)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.5")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
 
     // Test-Abhängigkeiten
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    //Gson
-    implementation("com.google.code.gson:gson:2.10.1")
-
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     //Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 
     //Coil
-    implementation("io.coil-kt:coil:2.7.0")
+    implementation(libs.coil)
+    implementation (libs.glide)
 
 
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation(libs.retrofit)
+
+    //Gson
+    annotationProcessor(libs.compiler)
+    implementation(libs.gson)
+    implementation(libs.converter.gson)
 
     // Moshi
-    implementation("com.squareup.moshi:moshi:1.15.1")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation(libs.moshi)
+    implementation(libs.converter.moshi)
+    implementation(libs.moshi.kotlin)
 
     // OkHttp
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
 
     //Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
-    implementation ("com.google.firebase:firebase-common-ktx") // Firebase Common KTX
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation(platform(libs.firebase.bom))
+    implementation (libs.firebase.common.ktx) // Firebase Common KTX
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore.ktx)
 }
 
 
